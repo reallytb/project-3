ea51fbf09951223b1d2730ddd1b41ee3991fa41a
reallytb <maks07092004@mail.ru>
2025-06-20 15:25:34 +0300

    first iteration

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,80 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	dir := filepath.Dir("handlers.go")
+	projectRoot := filepath.Join(dir, "..")
+	path, err := filepath.Abs(projectRoot)
+	path = filepath.Join(path, "index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("Форма запарсилась")
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("получен файл из формы")
+	defer file.Close()
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("файл прочитан")
+	convertedData := service.Convert(string(fileBytes))
+	log.Println("данные конвертированы")
+	outputFilename := fmt.Sprintf("result_%s%s",
+		time.Now().UTC().Format("20060102_150405"),
+		filepath.Ext(header.Filename))
+	newFile, err := os.Create(outputFilename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("файл создан")
+	defer newFile.Close()
+	err = os.WriteFile(outputFilename, []byte(convertedData), 0755)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println("файл записан")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(convertedData))
+	log.Println("---------------------------------")
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(data string) string {
+	morseChars := ".- "
+	isText := true
+	for _, char := range data {
+		if strings.ContainsRune(morseChars, char) {
+			isText = false
+		}
+	}
+	switch isText {
+	case true:
+		morse := morse.ToMorse(data)
+		return morse
+	case false:
+		text := morse.ToText(data)
+		return text
+	}
+	log.Fatal("ошибка конвертации текста/морзе")
+	fmt.Println("ошибка конвертации текста/морзе")
+	return ""
+}
